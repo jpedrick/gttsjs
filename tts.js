980e86a8b8e0de14666dde13c518c6d023839226
Joshua Pedrick <joshua.pedrick@gmail.com>
2019-09-17 00:17:47 -0400

    removed gtts_token as separate file

--- tts.js
+++ tts.js
@@ -13,13 +13,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./gtts_token", "request", "fs"], factory);
+        define(["require", "exports", "utf8", "request-promise-native", "fs"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    const gtts_token = require("./gtts_token");
-    const request = require("request");
+    const utf8 = require("utf8");
+    const request = require("request-promise-native");
     const fs = require("fs");
     const GOOGLE_TTS_MAX_CHARS = 100;
     const GOOGLE_TTS_URL = "https://translate.google.com/translate_tts";
@@ -34,7 +34,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
             this.speed = 1.0;
             this.token = null;
             this.speed = speed;
-            this.token = new gtts_token.Token();
+            this.token = new Token();
             this.lang = lang;
         }
         uri(text) {
@@ -72,6 +72,72 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
             });
         }
     }
+    function _rshift(a, d) {
+        return a >= 0 ? a >> d : (a + 0x100000000) >> d;
+    }
+    function _work_token(a, seed) {
+        var r = a;
+        for (var i = 0; i < seed.length - 2; i += 3) {
+            const c = seed[i + 2];
+            const d = c > 'a' ? +c - 87 : +c;
+            const e = seed[i + 1] == '+' ? _rshift(r, d) : r << d;
+            r = seed[i] == '+' ? r + e & 4294967295 : r ^ d;
+        }
+        return r;
+    }
+    class Token {
+        constructor() {
+            this.SALT_1 = "+-a^+6";
+            this.SALT_2 = "+-3^+b+-f";
+            this.token_key = null;
+        }
+        calculate_token(text, seed) {
+            return __awaiter(this, void 0, void 0, function* () {
+                console.log('seed: ' + seed);
+                if (seed == null) {
+                    seed = yield this._get_token_key();
+                    console.log('got seed from token_key: ' + seed);
+                }
+                if (seed) {
+                    const split_seed = seed.split(".");
+                    var first_seed = +split_seed[0];
+                    var second_seed = +split_seed[1];
+                    var d = Array.from(utf8.encode(text));
+                    var a = first_seed | 0;
+                    d.map(v => { a += +v; a = _work_token(a, this.SALT_1); });
+                    a = _work_token(a, this.SALT_2);
+                    a ^= second_seed;
+                    if (a <= 0) {
+                        a = (a & 0x8fff) + 0x8ffe;
+                    }
+                    a %= 1e6;
+                    console.log('a:' + a + ' first_seed:' + first_seed + ' a^first_seed:' + (a ^ first_seed));
+                    return String(a) + "." + String(a ^ first_seed);
+                }
+                return null;
+            });
+        }
+        _get_token_key() {
+            return __awaiter(this, void 0, void 0, function* () {
+                if (this.token_key != null) {
+                    return this.token_key;
+                }
+                const response = yield request.get({ uri: 'https://translate.google.com/' });
+                const tkk_expr = response.match("(tkk:.*?),");
+                const result = tkk_expr[1].match(/\d+\.\d+/)[0];
+                this.token_key = result;
+                return this.token_key;
+            });
+        }
+    }
+    exports.Token = Token;
+    var tkn = new Token();
+    const key = tkn._get_token_key().then(key => {
+        console.log("TokenKey: '" + key + "'");
+    });
+    tkn.calculate_token("hello world hello hello hello", null).then(function (tkn) {
+        console.log("Token: '" + tkn + "'");
+    });
     function test() {
         var gtts = new GTTS('en-in', 0.5);
         gtts.fetch_and_save("Ummm, Hello World, hello hello hello", 'hi.mp3');
